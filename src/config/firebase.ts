3894926abd9dfdf80964a3b8d6e54872a43881d5
IKUN-smallblackfan <breezechen3@gmail.com>
2026-05-25 12:34:32 +0800

    Restore default Firestore persistence for offline sync support

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { initializeFirestore, memoryLocalCache } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBl5G2FULAIDEIskS4tdmdTwPj6AMLORqI',
@@ -12,4 +12,4 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const db = initializeFirestore(app, { localCache: memoryLocalCache() })
+export const db = getFirestore(app)
